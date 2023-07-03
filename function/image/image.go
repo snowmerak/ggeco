305253616b818d7e680ced19c9ee9d9271b6bb28
snowmerak <snowmerak@outlook.com>
2023-07-04 00:09:34 +0900

    make image handler

---
+++ function/image/image.go
@@ -0,0 +1,21 @@
+package image
+
+import (
+	"github.com/snowmerak/ggeco/gen/bean"
+	"github.com/snowmerak/ggeco/lib/storage"
+	"net/http"
+)
+
+func Handler(container *bean.Container) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		filename := r.URL.Query().Get("name")
+		url, err := storage.GetSASURL(container, "image-silo", filename)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.Write([]byte(url))
+	}
+}
