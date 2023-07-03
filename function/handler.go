84d47f84601b48c4124cca4a4203af5232a9e379
snowmerak <snowmerak@outlook.com>
2023-07-04 00:09:41 +0900

    update handler

--- function/handler.go
+++ function/handler.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"context"
+	"github.com/snowmerak/ggeco/function/image"
 	"github.com/snowmerak/ggeco/function/place"
 	"github.com/snowmerak/ggeco/function/search"
 	"github.com/snowmerak/ggeco/gen/bean"
 	"github.com/snowmerak/ggeco/lib/maps"
+	"github.com/snowmerak/ggeco/lib/sqlserver"
+	"github.com/snowmerak/ggeco/lib/storage"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	listenAddr := ":8080"
 	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
 		listenAddr = ":" + val
@@ -23,8 +31,26 @@ func main() {
 	}
 	maps.PushClient(container, mapsClient)
 
+	sqlServerHost := os.Getenv("SQL_SERVER_HOST")
+	sqlServerPort, _ := strconv.ParseInt(os.Getenv("SQL_SERVER_PORT"), 10, 64)
+	sqlServerUser := os.Getenv("SQL_SERVER_USER")
+	sqlServerPassword := os.Getenv("SQL_SERVER_PASSWORD")
+	sqlServerDatabase := os.Getenv("SQL_SERVER_DATABASE")
+	sqlClient, err := sqlserver.New(ctx, sqlServerHost, int(sqlServerPort), sqlServerUser, sqlServerPassword, sqlServerDatabase)
+	if err != nil {
+		panic(err)
+	}
+	sqlserver.PushClient(container, sqlClient)
+
+	imageClient, err := storage.New(os.Getenv("AZURE_STORAGE_ACCOUNT"), os.Getenv("AZURE_STORAGE_ACCESS_KEY"))
+	if err != nil {
+		panic(err)
+	}
+	storage.PushClient(container, imageClient)
+
 	http.HandleFunc("/api/place", place.Handler(container))
 	http.HandleFunc("/api/search", search.Handler(container))
+	http.HandleFunc("/api/image", image.Handler(container))
 	log.Printf("About to listen on %s. Go to https://127.0.0.1%s/", listenAddr, listenAddr)
 	log.Fatal(http.ListenAndServe(listenAddr, nil))
 }
