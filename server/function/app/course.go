7bf169a42a6d6da3bace0c490dc1a8680af57bd3
snowmerak <snowmerak@outlook.com>
2023-08-12 03:07:22 +0900

    make get populars and recents

---
+++ server/function/app/course.go
@@ -0,0 +1,112 @@
+package app
+
+import (
+	"database/sql"
+	"errors"
+	"github.com/julienschmidt/httprouter"
+	"github.com/snowmerak/ggeco/server/gen/bean"
+	"github.com/snowmerak/ggeco/server/lib/client/sqlserver"
+	"github.com/snowmerak/ggeco/server/lib/service/courses"
+	"net/http"
+	"strconv"
+)
+
+type GetPopularCourseOfBadgeRequest struct {
+	BadgeId string `query:"badge_id"`
+	Count   int    `query:"count"`
+}
+
+type Course struct {
+	Id         string `json:"id"`
+	AuthorId   string `json:"author_id"`
+	Name       string `json:"name"`
+	RegDate    string `json:"reg_date"`
+	Review     string `json:"review"`
+	Favorites  int    `json:"favorites"`
+	IsFavorite bool   `json:"is_favorite"`
+}
+
+type GetPopularCourseOfBadgeResponse struct {
+	Courses []Course `json:"courses"`
+}
+
+func GetPopularCourseOfBadge(container bean.Container) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		badgeId := new(sqlserver.UUID)
+		if err := badgeId.From(r.URL.Query().Get("badge_id")); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		count, err := strconv.Atoi(r.URL.Query().Get("count"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		list, err := courses.GetPopularInBadge(container, *badgeId, count)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		result := GetPopularCourseOfBadgeResponse{}
+		for _, v := range list {
+			favoriteCount, err := courses.CountFavoriteCourse(container, v.Id)
+			if err != nil && errors.Is(err, sql.ErrNoRows) {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			result.Courses = append(result.Courses, Course{
+				Id:        v.Id.String(),
+				AuthorId:  v.AuthorID.String(),
+				Name:      v.Name,
+				RegDate:   v.RegDate,
+				Review:    v.Review,
+				Favorites: favoriteCount,
+			})
+		}
+	}
+}
+
+type GetRecentCoursesRequest struct {
+	Count int `query:"count"`
+}
+
+type GetRecentCoursesResponse struct {
+	Courses []Course `json:"courses"`
+}
+
+func GetRecentCourses(container bean.Container) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		count, err := strconv.Atoi(r.URL.Query().Get("count"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		list, err := courses.GetNewest(container, count)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		result := GetRecentCoursesResponse{}
+		for _, v := range list {
+			favoriteCount, err := courses.CountFavoriteCourse(container, v.Id)
+			if err != nil && errors.Is(err, sql.ErrNoRows) {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			result.Courses = append(result.Courses, Course{
+				Id:        v.Id.String(),
+				AuthorId:  v.AuthorID.String(),
+				Name:      v.Name,
+				RegDate:   v.RegDate,
+				Review:    v.Review,
+				Favorites: favoriteCount,
+			})
+		}
+	}
+}
