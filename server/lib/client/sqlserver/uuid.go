98695d3c670b1cc8577b6c08b5e510d7ffcfbe04
snowmerak <snowmerak@outlook.com>
2023-08-11 23:19:50 +0900

    update String and From method

--- server/lib/client/sqlserver/uuid.go
+++ server/lib/client/sqlserver/uuid.go
@@ -1,3 +1,18 @@
 package sqlserver
 
+import "encoding/base64"
+
 type UUID []byte
+
+func (u *UUID) String() string {
+	return base64.URLEncoding.EncodeToString(*u)
+}
+
+func (u *UUID) From(s string) error {
+	b, err := base64.URLEncoding.DecodeString(s)
+	if err != nil {
+		return err
+	}
+	*u = b
+	return nil
+}
