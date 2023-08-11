d0d9bb988a3f1ac980e337024362b6185a075df3
snowmerak <snowmerak@outlook.com>
2023-08-12 03:08:00 +0900

    make badges api

---
+++ server/function/app/badges.go
@@ -0,0 +1,49 @@
+package app
+
+import (
+	"encoding/json"
+	"github.com/julienschmidt/httprouter"
+	"github.com/snowmerak/ggeco/server/gen/bean"
+	"github.com/snowmerak/ggeco/server/lib/service/badges"
+	"net/http"
+)
+
+type GetBadgesRequest struct {
+}
+
+type Badge struct {
+	Id      string `json:"id,omitempty"`
+	Name    string `json:"name,omitempty"`
+	Summary string `json:"summary,omitempty"`
+	Image   string `json:"image,omitempty"`
+}
+
+type GetBadgesResponse struct {
+	Badges []Badge `json:"badges"`
+}
+
+func GetBadges(container bean.Container) httprouter.Handle {
+	return func(wr http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+		list, err := badges.GetList(container)
+		if err != nil {
+			http.Error(wr, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		result := GetBadgesResponse{}
+		for _, b := range list {
+			result.Badges = append(result.Badges, Badge{
+				Id:      b.Id.String(),
+				Name:    b.Name,
+				Summary: b.Summary,
+				Image:   b.Image,
+			})
+		}
+
+		encoder := json.NewEncoder(wr)
+		if err := encoder.Encode(result); err != nil {
+			http.Error(wr, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
