f84a2a079473039f5ce5fb9765bc7c45e7df9e7b
snowmerak <snowmerak@outlook.com>
2023-08-26 00:05:13 +0900

    make place types

---
+++ server/lib/service/badges/place_type.go
@@ -0,0 +1,65 @@
+package badges
+
+import (
+	"database/sql"
+	"encoding/hex"
+	"github.com/snowmerak/ggeco/server/gen/bean"
+	"github.com/snowmerak/ggeco/server/lib/client/sqlserver"
+	"golang.org/x/crypto/blake2s"
+)
+
+var _ = `
+CREATE TABLE [dbo].[PlaceTypeToBadgeId] (
+    [id]         UNIQUEIDENTIFIER NOT NULL,
+    [place_type] VARCHAR (64)    NOT NULL,
+    [badge_id]   UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_PlaceTypeToBadgeId] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+`
+
+func GetBadgeFromPlaceType(container bean.Container, pt string) (sqlserver.UUID, error) {
+	client, err := sqlserver.GetClient(container)
+	if err != nil {
+		return sqlserver.UUID{}, err
+	}
+
+	hashed := blake2s.Sum256([]byte(pt))
+	pt = hex.EncodeToString(hashed[:])
+
+	stmt, err := client.Prepare("SELECT [badge_id] from [dbo].[PlaceTypeToBadgeId] WHERE [place_type] = @P1")
+	if err != nil {
+		return sqlserver.UUID{}, err
+	}
+	defer stmt.Close()
+
+	row := stmt.QueryRow(pt)
+	if err := row.Err(); err != nil {
+		return sqlserver.UUID{}, err
+	}
+
+	var badgeId sqlserver.UUID
+	if err := row.Scan(&badgeId); err != nil {
+		return sqlserver.UUID{}, err
+	}
+
+	return badgeId, nil
+}
+
+func AddPlaceTypeToBadgeId(container bean.Container, pt string, badgeId sqlserver.UUID) error {
+	client, err := sqlserver.GetClient(container)
+	if err != nil {
+		return err
+	}
+
+	hashed := blake2s.Sum256([]byte(pt))
+	pt = hex.EncodeToString(hashed[:])
+
+	stmt, err := client.Prepare(`INSERT INTO [dbo].[PlaceTypeToBadgeId] ([place_type], [badge_id]) VALUES (@P1, @P2)`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(sql.Named("placeType", pt), sql.Named("badgeId", badgeId))
+	return err
+}
